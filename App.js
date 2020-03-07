03af5c379063d0c0d3f136434713f6d1ae6e1d8e
mukhlish <mukhlishanafi04@gmail.com>
2020-03-07 14:09:29 +0700

    test analytics

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {StyleSheet, View, Button} from 'react-native';
+import {StyleSheet, View, Button, Alert} from 'react-native';
 import Crashes from 'appcenter-crashes';
+import Analytics from 'appcenter-analytics';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -12,17 +13,31 @@ export default class App extends React.Component {
     const didCrash = await Crashes.hasCrashedInLastSession();
     if (didCrash) {
       const report = await Crashes.lastSessionCrashReport();
-      alert("Sorry about that crash, we're working on a solution");
+      Alert.alert(
+        'Sorry',
+        "Sorry about that crash, we're working on a solution",
+        [{text: 'Okay', onPress: () => console.log('pressed')}],
+      );
+      // console.log(report);
     }
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Button
+        {/* <Button
           title="Crash"
           onPress={() => {
             Crashes.generateTestCrash();
+          }}
+        /> */}
+        <Button
+          title="Calculate inflation"
+          onPress={() => {
+            Analytics.trackEvent('calculate_inflation', {
+              Internet: 'Cellular',
+              GPS: 'On',
+            });
           }}
         />
       </View>
